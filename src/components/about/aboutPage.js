21f4e043b65e319eab0c85d6cc354db2a4404029
Gaetano Santonastaso <gsantonastaso@salesforce.com>
2018-04-11 13:35:09 +0100

    Created author form

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,21 +3,39 @@
 var React = require('react');
 
 var About = React.createClass({
+    statics: {
+        willTransitionTo: function(transition, params, query, callback) {
+            if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
+                transition.abort();
+            } else {
+                callback();
+            }
+        },
+
+        willTransitionFrom: function(transition, component) {
+            if (!confirm('Are you sure you want to leave a page that\'s this exciting?')) {
+                transition.abort();
+            }
+        }
+    },
     render: function () {
         return (
             <div>
                 <h1>About</h1>
-                <p>Rect, React router, and Flux</p>
                 <p>
-                    This application uses
+                    This application uses the following technologies:
                     <ul>
                         <li>React</li>
                         <li>React Router</li>
                         <li>Flux</li>
-                        <li>.....</li>
+                        <li>Node</li>
+                        <li>Gulp</li>
+                        <li>Browserify</li>
+                        <li>Bootstrap</li>
                     </ul>
                 </p>
-            </div>);
+            </div>
+        );
     }
 });
 
