b037b8dd90a48641b5ef3b1d9746987e563ff619
Gaetano Santonastaso <gsantonastaso@salesforce.com>
2018-04-11 16:54:22 +0100

    added flux store

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -3,8 +3,8 @@
 var React = require('react');
 var Router = require('react-router');
 var AuthorForm = require('./authorForm');
-// var AuthorActions = require('../../actions/authorActions');
-// var AuthorStore = require('../../stores/authorStore');
+var AuthorActions = require('../../actions/authorActions');
+var AuthorStore = require('../../stores/authorStore');
 var toastr = require('toastr');
 
 var ManageAuthorPage = React.createClass({
@@ -31,8 +31,7 @@ var ManageAuthorPage = React.createClass({
     componentWillMount: function() {
         var authorId = this.props.params.id; //from the path '/author:id'
         if (authorId) {
-
-           // this.setState({author: AuthorStore.getAuthorById(authorId) });
+           this.setState({author: AuthorStore.getAuthorById(authorId) });
         }
     },
 
@@ -70,9 +69,9 @@ var ManageAuthorPage = React.createClass({
         }
 
         if (this.state.author.id) {
-       //     AuthorActions.updateAuthor(this.state.author);
+           AuthorActions.updateAuthor(this.state.author);
         } else {
-       //     AuthorActions.createAuthor(this.state.author);
+           AuthorActions.createAuthor(this.state.author);
         }
 
         this.setState({dirty: false});
